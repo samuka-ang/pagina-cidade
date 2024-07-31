d33c823263e4ba0d37a55d66afd1825bb25d9867
Samuel Angulo <159262007+samuka-ang@users.noreply.github.com>
2024-07-31 10:42:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = '1.png'
+    legendas.innerHTML= 'Costa De Lima'
+    
+}
+function imagem2(){
+
+    lamp. src = '2.jpg'
+    legendas.innerHTML= 'Huaca Pucllana'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '3.jpg'
+    legendas.innerHTML= 'Praça Maior De Lima'
+}
+function imagem4(){
+
+    lamp. src = '4.jpg'
+    legendas.innerHTML= 'Basílica Menor'
+}
